d8971febf04c3934788fae3f5e973525df28f4a6
Ray Ortiz <raymundo.ortiz@yahoo.com>
2017-02-16 22:53:03 -0600

    Initial maven structure, still lot of work to do...but we have progress :)

---
+++ src/main/java/com/accenture/labs/Application.java
@@ -0,0 +1,15 @@
+package com.accenture.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by raymundoortiz on 2/16/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
